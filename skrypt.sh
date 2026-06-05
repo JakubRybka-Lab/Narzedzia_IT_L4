9d6f073efdf8ce12827652331e7c89e774a6a426
JakubRybka-Lab <marmonjak1978@gmail.com>
2026-06-05 17:15:26 +0200

    Dodano flagi -h, -l, -d

--- skrypt.sh
+++ skrypt.sh
@@ -3,23 +3,25 @@
 SCRIPT_NAME=$(basename "$0")
 show_help() {
 	echo "Użycie: ./SCRIPT_NAME [OPCJE}"
-	echo " --date 	Wyświetla dzisiejszą date"
-	echo " --logs [N] Tworzy N plików log ( domyślnie 100)"
-	echo " --help	Wyświetla pomoc"
+	echo " -d, --date 	Wyświetla dzisiejszą date"
+	echo " -l, --logs [N] Tworzy N plików log ( domyślnie 100)"
+	echo " -h, --help	Wyświetla pomoc"
 }
-if [ "$1" == "--help" ] || [ $# -eq 0 ];then
-	show help
-elif ["$`" == "--date" ]; then
-	date
-elif [ "$1" == "--logs" ]; then
-	NUM=100
-	if [[ -n "$2" && "$2" =~ ^[0-9+$ ]]; then
-	NUM="$2"
-fi
+if [ $# -eq 0 ]; then show_help; exit 0; fi
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+	-d|--date) date; shift ;;
+	-l|--logs)
+	    NUM=100
+	if [[ -n "$2" && "$2" =~ ^[0-9+$ ]]; then NUM="$2"; shift; fi
 for ((i=1; i<=NUM; i++)); do
 	FILENAME="log${i}.txt"
 	echo "Nazwa pliku: $Nazwa pliku: $FILENAME" > "$FILENAME"
 	echo "Skrypt: $SCRIPT_NAME" >> "$FILENAME"
 	echo "Data: $(date)" >> "$FILENAME"
-	done
-fi
+done
+    shift ;;
+    -h|--help) show_help; shift ::
+    *) shift ;;
+ esac
+done
